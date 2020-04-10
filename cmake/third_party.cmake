79df236e2a17dbd129c3ae9ebc6e3857cae975a4
lcy-seso <lcy.seso@gmail.com>
2020-04-11 02:40:39 +0800

    clang hello world test,

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -10,6 +10,23 @@ set(THIRD_PARTY_CACHE_PATH
 
 set(THIRD_PARTY_BUILD_TYPE Release)
 
+# ~~~
+# cache funciton to avoid repeat download code of third_party.
+# This function has 4 parameters, URL / REPOSITOR / TAG / DIR:
+# 1. URL:           specify download url of 3rd party
+# 2. REPOSITORY:    specify git REPOSITORY of 3rd party
+# 3. TAG:           specify git tag/branch/commitID of 3rd party
+# 4. DIR:           overwrite the original SOURCE_DIR when cache directory
+#
+# The function Return 1 PARENT_SCOPE variables:
+#  - TARGETDOWNLOADCMD:Simplyplace"{TARGET}_DOWNLOAD_CMD" in ExternalProject_Add,
+#                            and you no longer need to set any donwnload steps in ExternalProject_Add.
+# For example:
+#    Cache_third_party(${TARGET}
+#            REPOSITORY ${TARGET_REPOSITORY}
+#            TAG        ${TARGET_TAG}
+#            DIR        ${TARGET_SOURCE_DIR})
+# ~~~
 function(cache_third_party TARGET)
   set(options "")
   set(oneValueArgs URL REPOSITORY TAG DIR)
